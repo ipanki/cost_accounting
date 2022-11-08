19b80d1aa490353370259fa7ffa12d8307838da5
Daniil Panko <daniil.panko@mail.ru>
2022-11-08 14:23:31 +0300

    Change to ModelViewSet at categories_views

--- manager/catogories_views.py
+++ manager/catogories_views.py
@@ -1,39 +1,24 @@
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
-from rest_framework import status
+from rest_framework import status, viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ViewSet
 from manager.serializers import ShowCategoriesSerializer, EditCategoriesSerializer
 from manager.models import Tag
 
 
-class CategoriesViewSet(ViewSet):
+class CategoriesViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticated,)
+    serializer_class = ShowCategoriesSerializer
+    queryset = Tag.objects
 
-    def list(self, request):
-        """Show all categories"""
-        categories = Tag.objects.filter(user=request.user)
-        serializer = ShowCategoriesSerializer(categories, many=True)
-        return Response({'data': serializer.data}, status=status.HTTP_200_OK)
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
 
-    def update(self, request, pk):
-        tag = get_object_or_404(Tag, pk=pk)
-        form = EditCategoriesSerializer(data=request.data, instance=tag)
-        form.is_valid(raise_exception=True)
-        profile = form.save()
-        return Response(status=status.HTTP_201_CREATED, data=EditCategoriesSerializer(profile).data)
+    def get_serializer_class(self):
+        if self.action == 'create' or self.action == 'update':
+            return EditCategoriesSerializer
+        return super().get_serializer_class()
 
-    def create(self, request):
-        """Create a new page"""
-        tag = EditCategoriesSerializer(data=request.data)
-        tag.is_valid(raise_exception=True)
-        profile = tag.save(user=request.user)
-        return Response(status=status.HTTP_201_CREATED, data=EditCategoriesSerializer(profile).data)
-
-    def delete(self, request, pk):
-        tag = get_object_or_404(Tag, pk=pk)
-        if request.user == tag.user:
-            tag.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
-        else:
-            return Response(status=status.HTTP_403_FORBIDDEN)
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
